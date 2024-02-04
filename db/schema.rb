36415d3ab183954cfd7bad2d1ae94cc0c6ff1b1a
K93wyuta <k93wyuta@gmail.com>
2024-02-04 18:19:08 +0900

    プルダウンの実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_04_082118) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_04_085138) do
   create_table "room_users", charset: "utf8", force: :cascade do |t|
     t.bigint "room_id", null: false
     t.bigint "user_id", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_04_082118) do
   create_table "rooms", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
